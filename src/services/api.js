f1ef10deaa88d727e3561ce720b3f7d7895ced49
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-07-16 23:09:17 +0900

    newnewbranch

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://6eab-2001-e60-a302-701b-cc6e-366d-a294-aacd.ngrok-free.app/api',
+  baseURL: 'https://7478-2001-e60-a302-701b-cc6e-366d-a294-aacd.ngrok-free.app/api',
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': '69420',
